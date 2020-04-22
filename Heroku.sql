16f1739a9d351e384b8d719bee400809b7446d93
shamlin143 <shamlin@gvtc.com>
2020-04-22 13:55:30 -0500

    Create Heroku.sql

---
+++ Heroku.sql
@@ -0,0 +1,17 @@
+USE szedbvd4lic4v72b;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	date TIMESTAMP,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO burgers (burger_name) VALUES ('Fat Burger');
+INSERT INTO burgers (burger_name) VALUES ('Really Fat Burger');
+INSERT INTO burgers (burger_name) VALUES ('Obese Killer Burger');
+
+
+SELECT * FROM burgers;
